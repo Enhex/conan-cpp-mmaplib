2b6f7335de794043eb7cf490fb22a70d61dd35a5
Enhex <enhex0@gmail.com>
2017-12-12 13:35:24 +0200

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conans import ConanFile, tools
+import os
+
+
+class CppmmaplibConan(ConanFile):
+	name = "cpp-mmaplib"
+	version = "master"
+	license = "MIT"
+	url = ""
+	description = "C++11 header-only memory mapped file library."
+	# No settings/options are necessary, this is header only
+
+	def source(self):
+		self.run("git clone --depth 1 https://github.com/Enhex/cpp-mmaplib.git")
+		
+	def package(self):
+		self.copy("*.h", src='cpp-mmaplib', dst='include')
---
+++ test_package/example.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <mmaplib.h>
+
+int main() {	
+	mmaplib::MemoryMappedFile mapped_file("");
+	std::puts("package test complete.");
+}
